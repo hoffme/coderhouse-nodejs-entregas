e36b3e2342de491c6f63feb5b2fdb69e95ae56fc
Juan Ignacio Coronel <phjocoronel@gmail.com>
2021-07-13 20:12:12 -0300

    entrega-20

--- entrega-8-a-13-y-17/controllers/index.js
+++ entrega-8-a-13-y-17/controllers/index.js
@@ -1,39 +1,43 @@
 import ProductsController from "./product.js";
 import ChatController from "./chat.js";
 
-import KnexRepository from "../storage/repositories/knex.js";
+import MongooseRepository from "../storage/repositories/mongoose.js";
+import mongoConnection from "../storage/connections/mongo.js";
 
-import mysqlConnection from "../storage/connections/mysql.js";
+import mongoose from 'mongoose';
 
 class Controllers {
     static products = undefined;
     static chat = undefined;
 
     static async setup() {
-        const productsRepository = new KnexRepository(mysqlConnection, {
-            name: 'productos',
-            builder: async (table) => {
-                table.increments('id', { primaryKey: true })
-                table.string('name')
-                table.string('thumbnail')
-                table.float('price')
-            }
-        });
-        await productsRepository.setup();
+        const ObjectIdSchema = mongoose.Schema.ObjectId;
+        const ObjectId = mongoose.Types.ObjectId;
 
+        const productsRepository = new MongooseRepository(
+            mongoConnection,
+            'productos',
+            new mongoose.Schema({
+                id: {type: ObjectIdSchema, default: new ObjectId()},
+                name: {type: String, required: true, max: 100},
+                thumbnail: {type: String, max: 200},
+                price: {type: Number, required: true},
+            })
+        )
+        await productsRepository.setup();
         Controllers.products = new ProductsController(productsRepository);
 
-        const chatRepository = new KnexRepository(mysqlConnection, {
-            name: 'chat',
-            builder: async (table) => {
-                table.increments('id', { primaryKey: true })
-                table.string('email')
-                table.string('text')
-                table.date('date')
-            }
-        });
+        const chatRepository = new MongooseRepository(
+            mongoConnection,
+            'chat',
+            new mongoose.Schema({
+                id: {type: ObjectIdSchema, default: new ObjectId()},
+                email: {type: String, required: true, max: 100},
+                text: {type: String, max: 200, required: true},
+                date: {type: Date, required: true},
+            })
+        )
         await chatRepository.setup();
-
         Controllers.chat = new ChatController(chatRepository);
     }
 }
